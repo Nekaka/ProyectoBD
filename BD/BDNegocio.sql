b4d77591fbd0ec897168add2fd132af04bfdc244
Esteban Barrera Pino <estebanbarrera132@gmail.com>
2020-11-23 23:41:16 -0300

    Se agregaron mas tablas a la BD

--- BD/BDNegocio.sql
+++ BD/BDNegocio.sql
@@ -21,8 +21,16 @@ CREATE TABLE ventas(
     id INT AUTO_INCREMENT,
     id_user INT,
     total INT,
-    id_producto INT,
+    id_producto VARCHAR(20),
     PRIMARY KEY (id),
     FOREIGN KEY (id_producto) REFERENCES productos(id),
     FOREIGN KEY (id_user) REFERENCES usuario(id)
+);
+
+CREATE TABLE pedido(
+    id INT AUTO_INCREMENT,
+    producto VARCHAR(50),
+    cantidad INT(6),
+    PRIMARY KEY (id),
+    FOREIGN KEY (producto) REFERENCES productos(id)
 );
