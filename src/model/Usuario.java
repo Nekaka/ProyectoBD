a0031d5468768e166ef8e1e382edf1bb9c945292
Esteban Barrera Pino <estebanbarrera132@gmail.com>
2020-11-24 23:03:59 -0300

    Se termina model usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -15,4 +15,49 @@ public class Usuario {
     private String pass;
     private String nom_ape;
     
+    public Usuario(){
+        
+    }
+    
+    public Usuario(int id, String user, String pass, String nom_ape){
+        this.id = id;
+        this.user = user;
+        this.pass = pass;
+        this.nom_ape = nom_ape;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getNom_ape() {
+        return nom_ape;
+    }
+
+    public void setNom_ape(String nom_ape) {
+        this.nom_ape = nom_ape;
+    }
+    
+    
+    
 }
