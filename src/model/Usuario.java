31ec4cb65f32dc1f48a72d2ab23f5af82a74be57
Esteban Barrera Pino <estebanbarrera132@gmail.com>
2020-11-24 23:02:09 -0300

    Se crea package model y el model de los usuarios

---
+++ src/model/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Nekaka
+ */
+public class Usuario {
+    private int id;
+    private String user;
+    private String pass;
+    private String nom_ape;
+    
+}
