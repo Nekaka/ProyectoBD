20457e8253db5bd977d7efb0511badc9ca683e84
vania suarez <vania85@hotmail.cl>
2020-11-25 08:16:25 -0300

    Se agregan lo que faltaba del model productos

--- src/model/Productos.java
+++ src/model/Productos.java
@@ -23,4 +23,37 @@ public class Productos {
         this.stock = stock;
         this.precio = precio;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+     
 }
