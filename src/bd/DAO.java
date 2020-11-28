0482c7e678316c020c118f845b50bed372a96c02
Esteban Barrera Pino <estebanbarrera132@gmail.com>
2020-11-27 23:04:26 -0300

    Se crea crearUsuario en el DAO

--- src/bd/DAO.java
+++ src/bd/DAO.java
@@ -5,14 +5,22 @@
  */
 package bd;
 import java.sql.SQLException;
+import model.Usuario;
 /**
  *
  * @author vania
  */
 public class DAO {
-private Conexion oConexion;
-public DAO() throws SQLException {
-        oConexion = new Conexion("localhost", "BDnegocio", "root", "");
+    
+    private Conexion oConexion;
+    
+    public DAO() throws SQLException {
+            oConexion = new Conexion("localhost", "BDnegocio", "root", "");
+        }
+    
+    public void crearUsuario(Usuario oUsuario) throws SQLException  {
+        String sql = "INSERT INTO usuario (null, '" + oUsuario.getUser() +"', '" + oUsuario.getPass() +"', '" + oUsuario.getNom_ape() + "');";
+        
     }
     
 }
