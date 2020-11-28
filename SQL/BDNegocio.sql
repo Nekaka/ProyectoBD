4246ee440b4aaa44696c2a59694b1c14bd0eff0b
vania suarez <vania85@hotmail.cl>
2020-11-28 20:02:28 -0300

    modificaciones en tablas de la BD

--- SQL/BDNegocio.sql
+++ SQL/BDNegocio.sql
@@ -13,6 +13,7 @@ CREATE TABLE productos(
     nombre TEXT(20),
     stock INT(10),
     precio INT(10),
+    estado INT (1),
     PRIMARY KEY (id),
     UNIQUE (id)
 );
@@ -21,9 +22,9 @@ CREATE TABLE ventas(
     id INT AUTO_INCREMENT,
     id_user INT,
     total INT,
-    id_producto VARCHAR(20),
+    fecha_venta DATETIME,
+    estado INT(1),
     PRIMARY KEY (id),
-    FOREIGN KEY (id_producto) REFERENCES productos(id),
     FOREIGN KEY (id_user) REFERENCES usuario(id)
 );
 
@@ -37,7 +38,12 @@ CREATE TABLE pedido(
 CREATE TABLE boletas(
   id int AUTO_INCREMENT,
   id_ventas INT,
+  id_producto INT,
+  cantidad INT(11),
+  precio_venta INT(11),
   PRIMARY KEY (id),
-  FOREIGN KEY (id_ventas) REFERENCES ventas(id)    
+  FOREIGN KEY (id_ventas) REFERENCES ventas(id),  
+  FOREIGN KEY (id_producto) REFERENCES productos (id)
+
 
 );
